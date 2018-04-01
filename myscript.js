abac3eb7a94bf43960c9c3fd6fb48a5207b1d334
up2code <uppban5@gmail.com>
2018-04-01 21:03:56 +0700

    Initial commit

---
+++ myscript.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+		var latIconPack =  "url(https://puu.sh/p4JIR/bf5a4e3d39.png)";
+		var displayReact = "url(https://puu.sh/pFzaF/aee9e4317f.png)";
+		$(this).on('DOMNodeInserted', function(e) {
+				$('._2p7a > ._9--').css({"background-image":displayReact});
+				$('._2jry > ._9-_').css({"background-image":latIconPack});
+  	});
+});
